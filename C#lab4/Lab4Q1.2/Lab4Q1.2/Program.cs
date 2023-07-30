e711dd53586c28dec5d44987d4b6dae202de57b4
dasiya12345 <136570550+dasiya12345@users.noreply.github.com>
2023-07-30 15:46:40 +0530

    Add files via upload

---
+++ C#lab4/Lab4Q1.2/Lab4Q1.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Q2
+{
+    class Program
+    {
+        static void Main()
+        {
+            ConvertValues converter = new ConvertValues();
+
+            Console.Write("Enter the distance in kilometers (km): ");
+            if (double.TryParse(Console.ReadLine(), out double kilometers))
+            {
+                converter.KilometerToMeter(kilometers);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid numeric value for kilometers.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class ConvertValues
+    {
+        public void KilometerToMeter(double kilometers)
+        {
+            double meters = kilometers * 1000;
+            Console.WriteLine($"The distance in meters (m) is: {meters} m");
+        }
+    }
+}
---
+++ C#lab4/Lab4Q2.1/Lab4Q2.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        FindValues calculator = new FindValues();
+
+        Console.Write("Enter the radius of the circle: ");
+        if (double.TryParse(Console.ReadLine(), out double radius))
+        {
+            double area = calculator.FindArea(radius);
+            double circumference = calculator.FindCircumference(radius);
+
+            Console.WriteLine($"Area of the circle: {area}");
+            Console.WriteLine($"Circumference of the circle: {circumference}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid numeric value for the radius.");
+        }
+
+        Console.ReadLine();
+    }
+}
+
+class FindValues
+{
+    public double FindArea(double radius)
+    {
+        return 3.14 * radius * radius;
+    }
+
+    public double FindCircumference(double radius)
+    {
+        return 2 * 3.14 * radius;
+    }
+}
