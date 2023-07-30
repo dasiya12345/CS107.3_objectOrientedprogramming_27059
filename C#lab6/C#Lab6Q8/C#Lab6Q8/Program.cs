6458ad83f9bbd361e4af8d50c0e37bc754045011
dasiya12345 <136570550+dasiya12345@users.noreply.github.com>
2023-07-30 15:49:08 +0530

    Add files via upload

---
+++ C#lab6/C#Lab6Q8/C#Lab6Q8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_Q8
+{
+    class Animal
+    {
+        public void Display()
+        {
+            Console.WriteLine("I am an Animal");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void DisplayDogInfo()
+        {
+            Console.WriteLine("I have four legs");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+
+            animal.Display();
+            dog.DisplayDogInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#lab6/C#lab6Q6/C#lab6Q6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_Q6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array: ");
+            if (int.TryParse(Console.ReadLine(), out int size) && size > 0)
+            {
+                ArrayManipulator manipulator = new ArrayManipulator();
+
+                manipulator.ProcessArray(size);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer for the array size.");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+
+    class ArrayManipulator
+    {
+        public void ProcessArray(int size)
+        {
+            int[] array = new int[size * 2];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Enter value {i + 1}: ");
+                if (int.TryParse(Console.ReadLine(), out int input))
+                {
+                    array[i * 2] = input;
+                    array[i * 2 + 1] = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid numeric value.");
+                    i--;
+                }
+            }
+
+            Console.WriteLine("Array values:");
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
