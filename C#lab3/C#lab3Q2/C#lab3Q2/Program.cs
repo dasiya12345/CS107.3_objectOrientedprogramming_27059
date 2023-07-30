99e4d562031f44e33418f7aa6cdd603b45a5fdb6
dasiya12345 <136570550+dasiya12345@users.noreply.github.com>
2023-07-30 15:14:29 +0530

    Add files via upload

---
+++ C#lab3/C#lab3Q2/C#lab3Q2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            Console.Write("Enter a string: ");
+            str = Console.ReadLine();
+
+            int vowelCount = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+                {
+                    vowelCount++;
+                }
+            }
+
+            Console.Write("\nThe number of vowels in this string is {0}.", vowelCount);
+
+            Console.ReadKey();
+        }
+    }
+}
